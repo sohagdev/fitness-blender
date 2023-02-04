779a7d51b2e6b3fefb01e50f2d19c5d3e4284b3a
Sohag Himon <sohagdev19@gmail.com>
2023-02-04 22:29:14 +0600

    3rd

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,13 @@ import { Link } from 'react-router-dom'
 import logo from '../assets/images/Logo.png'
 const Navbar = () => {
   return (
-    <nav>
+    <nav className='py-5'>
       <Link to='/'>
-        <img src={logo} alt='logo' />
+        <img
+          src={logo}
+          alt='logo'
+          style={{ width: '48px', height: '48px', margin: '0 20px' }}
+        />
       </Link>
     </nav>
   )
